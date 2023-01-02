8056e6313832dba91e1b72728aaa2d9ef4529b8f
haruto.matsuda <haruto.matsuda@gmail.com>
2023-01-02 20:32:30 +0900

    認証前とメール認証と認証後のそれぞれのパスの設定

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ValidationController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//打刻ページ
+Route::get('/', [AttendanceController::class, 'index'])->middleware('auth')->middleware('verified')->name('index');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::post('/punch_in', [AttendanceController::class, 'punchIn'])->name('punch_in');
+Route::post('/punch_out', [AttendanceController::class, 'punchOut'])->name('punch_out');
+Route::post('/break_start', [AttendanceController::class, 'breakStart'])->name('break_start');
+Route::post('/break_end', [AttendanceController::class, 'breakEnd'])->name('break_end');
+
+//日付別勤怠ページ
+Route::get('/attendance', [AttendanceController::class, 'attendance'])->middleware('auth')->name('attendance');
+Route::get('/attendance_nextdate', [AttendanceController::class, 'attendanceNextdate'])->name('attendance_nextdate');
+Route::get('/attendance_beforedate', [AttendanceController::class, 'attendanceBeforedate'])->name('attendance_beforedate');
+
+
+Route::group(['middleware' => 'auth'], function () {
+    //ユーザー一覧ページ
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    //ユーザーごとの勤怠表が見れるページ
+    Route::get('/user/show/{id}', [UserController::class, 'show'])->name('user.show');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
